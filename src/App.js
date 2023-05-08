e2228a1a16043c9f6ede11b49dd844d64939bb0d
unknown <mnomanwd@gmail.com>
2023-05-08 04:58:14 -0700

    done
    done

--- src/App.js
+++ src/App.js
@@ -1,58 +1,76 @@
 import React, { useState, useEffect } from "react";
-import SingleColor from "./SingleColor";
-
-import Values from "values.js";
-
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+import data from "./data";
 function App() {
-  const [color, setColor] = useState("#fe1260");
-  const [list, setList] = useState([]);
-  const [isError, setIsError] = useState(false);
+  let [value, setValue] = useState(0);
+  const [reviews, setReviewsss] = useState(data);
+
   useEffect(() => {
-    const colors = new Values(color).all(10);
-    setList(colors);
-  }, []);
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    try {
-      const colors = new Values(color).all(10);
-      setList(colors);
-      setIsError(false);
-    } catch {
-      setIsError(true);
+    const lastReview = reviews.length - 1;
+    if (value < 0) {
+      setValue(lastReview);
     }
-  };
-  // const colors = color.all(10);
+    if (value >= reviews.length) {
+      setValue(0);
+    }
+  }, [value, reviews]);
+  useEffect(() => {
+    const slider = setInterval(() => {
+      setValue(value + 1);
+    }, 3000);
+    return () => {
+      clearInterval(slider);
+    };
+  }, [value]);
 
   return (
-    <>
-      <div>
-        <form className="container">
-          <h3> color generator</h3>
-          <input
-            className={isError ? "error" : ""}
-            type="text"
-            placeholder="#c5e5fc"
-            onChange={(e) => {
-              setColor(e.target.value);
-            }}
-          />
-          <button type="submit" className="btn" onClick={handleSubmit}>
-            submit
-          </button>
-        </form>
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span> review slider <span>/</span>
+        </h2>
       </div>
-      <article className="colors">
-        {list.map((color, colorIndex) => {
+      <div
+        className="section-center
+      "
+      >
+        {reviews.map((review, reviewIndex) => {
+          const { id, image, title, name, quote } = review;
+          let classPostion = "nextSlide";
+          if (reviewIndex === value) {
+            classPostion = "activeSlide";
+          }
+          if (
+            reviewIndex === value - 1 ||
+            (value === 0 && reviewIndex === reviews.length - 1)
+          ) {
+            classPostion = "lastSlide";
+          }
           return (
-            <SingleColor
-              key={colorIndex}
-              color={color}
-              colorIndex={colorIndex}
-            />
+            <article key={id} className={classPostion}>
+              <img src={image} alt="" className="person-img" />
+
+              <h4>{name}</h4>
+              <div className="title">{title}</div>
+              <div className="text">
+                <p> {quote}.</p>
+              </div>
+
+              <FaQuoteRight className="icon" />
+            </article>
           );
         })}
-      </article>
-    </>
+        <FiChevronLeft
+          className="icon prev"
+          onClick={() => setValue(value - 1)}
+        />
+        <FiChevronRight
+          className="icon next"
+          onClick={() => setValue(value + 1)}
+        />
+      </div>
+    </section>
   );
 }
 
